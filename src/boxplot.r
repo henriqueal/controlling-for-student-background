ec9d954a18c179fbc878b16159df64a1e9d69673
henriqueal <henriquebrmg@gmail.com>
2018-04-14 18:06:43 -0300

    v1 of boxplot graph

---
+++ src/boxplot.r
@@ -0,0 +1,19 @@
+#Graph:1 - Boxplot
+third_grade = read.csv("../input/third_grade.csv")
+fourth_grade = read.csv("../input/fourth_grade.csv")
+fifth_grade = read.csv("../input/fifth_grade.csv", na.strings="-")
+
+print(third_grade)
+print(fourth_grade)
+print(fifth_grade)
+
+boxplot(third_grade$Math, fourth_grade$Math, fifth_grade$Math, third_grade$Reading, fourth_grade$Reading, fifth_grade$Reading, 
+names = c("3","4", "5", "3","4", "5"), xlab="Math\t\t\t\t\t\t\tReading", ylab="Data", ylim=c(150,230), at=c(1,2,3,6,7,8))
+axis(2,at=c(150,160,170,180,190,200,210,220,230))
+title(xlab = "Grade", line = 4)
+
+# Mudar a cor do grafico
+# O eixo y está de ponta cabeça
+# O \t está muito feio
+# deixar o frafico mais comprido
+# eixo y: colocar o 150 como se fosse o ponto 0...
